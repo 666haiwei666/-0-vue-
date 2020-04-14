88cf3a393d059052c4b0acfd5d4b8a3b11641603
aline.chen <aline.chen@dbappsecurity.com.cn>
2020-04-14 21:31:05 +0800

    project init

---
+++ build/webpack.base.js
@@ -0,0 +1,68 @@
+// 使用node的path模块
+const path = require("path");
+
+// 引入vue-loader插件
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  // 打包的出口
+  // devServer配置
+  devtool: "inline-source-map",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      // 图片必须引入才会被打包  以http形式
+      // {
+      //   test: /\.(jpg|jpeg|png|svg)$/,
+      //   loader: "file-loader",
+      //   options: {   // 使文件保存原名
+      //     name: "[name].[ext]",
+      //   },
+      // },
+
+      //url-loader 直接将小图片打包以 base64 打包在 js 中, 减少 Http 请求的次数, 提高访问效率
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]",
+          limit: 2048 //当文件小于 2048byte 时, 以 base64 打包到 js 中, 当文件大于 2048byte 时, 使用 file-loader 打包
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "stylus-loader"]
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.js"
+    }
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!--  -->
+<template>
+  <div class="">
+    <h1>hello world 热加载543222</h1>
+    <!-- <img :src="img" alt=""> -->
+    <!-- // TODO 图片路径显示问题 -->
+    <input type="text" v-model="content" />
+    <button @click="addTodo">添加</button>
+    <ul>
+      <li v-for="item of todoData" :key="item.id">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import img from "../assets/images/1.png";
+import "../assets/styles/global.styl";
+// import '../assets/styles/test.css'
+export default {
+  components: {},
+  data() {
+    return {
+      todoData: ["todo1", "todo2", "todo3"],
+      content: ""
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    addTodo() {
+      if (this.content === "") return;
+
+      this.todoData.push(this.content);
+      this.content = "";
+    }
+  },
+  created() {
+    console.log(1111111111111111);
+    console.log(img)
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang="stylus" scoped>
+h1
+  color: red;
+  transform: translate(10px,10px);
+</style>
